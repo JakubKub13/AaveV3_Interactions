336e43b1340efc01cfa1bc2f11bcc3619a300dbf
JakubKub13 <jakubkubala3@gmail.com>
2022-11-25 09:14:12 +0100

    Setting up test state vars

--- test/aave_add_remove_liquidity.test.ts
+++ test/aave_add_remove_liquidity.test.ts
@@ -6,3 +6,10 @@ import { ethers, network } from "hardhat";
 const AAVE_POOL_ADDRESSES_PROVIDERV3 = "0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb"; //polygon
 const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
 const DAI_WHALE = "0x69CFAFcA9c889D6Efc1ACc5651ce7A5ec6CdC231";
+
+describe("Test Aave provide and withdraw liquidity", function () {
+    let accounts: SignerWithAddress[];
+    let dai: Contract;
+    let daiWhale: SignerWithAddress;
+    let aavePool: Contract;
+})
