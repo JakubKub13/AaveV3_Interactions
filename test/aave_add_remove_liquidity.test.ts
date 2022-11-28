fdfb0c5ac4942c83cee07421c7165b8843d9981d
JakubKub13 <jakubkubala3@gmail.com>
2022-11-28 10:06:11 +0100

    Testing interest accumulation

--- test/aave_add_remove_liquidity.test.ts
+++ test/aave_add_remove_liquidity.test.ts
@@ -7,7 +7,7 @@ import { AaveV3Interactions } from "../typechain-types";
 
 const AAVE_POOL_ADDRESSES_PROVIDERV3 = "0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb"; //polygon
 const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
-const aDAI = "0x27F8D03b3a2196956ED754baDc28D73be8830A6e";
+const aDAI = "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE";
 const DAI_WHALE = "0x075e72a5eDf65F0A5f44699c7654C1a76941Ddc8";
 const AMOUNT_APPROVE = 2000n * 10n ** 18n // 2000 DAI
 const AMOUNT_SUPPLY = 1000n * 10n ** 18n // 2000 DAI
@@ -94,6 +94,28 @@ describe("Test Aave provide and withdraw liquidity", function () {
             expect(ethers.utils.formatEther(balanceConDaiAfW)).to.eq("1000.0");
         });
 
-        
+        it("Should be accumulate interest after deposit", async () => {
+            const sendTx = await dai.connect(accounts[0]).transfer(aaveV3Interactions.address, AMOUNT_SUPPLY);
+            await sendTx.wait();
+            const pool = await aaveV3Interactions.aavePool();
+            const balanceOfDaiInContract = await dai.balanceOf(aaveV3Interactions.address);
+            const balanceOfDaiInContractFormatted = ethers.utils.formatEther(balanceOfDaiInContract);
+            expect(balanceOfDaiInContractFormatted).to.eq("1000.0");
+            const addLiqTx = await aaveV3Interactions.connect(accounts[0]).supplyLiquidity(dai.address, AMOUNT_SUPPLY);
+            await addLiqTx.wait();
+            const balanceContractBefW = await dai.balanceOf(aaveV3Interactions.address);
+            expect(ethers.utils.formatEther(balanceContractBefW)).to.eq("0.0")
+            await network.provider.send("evm_increaseTime", [360]);
+            await network.provider.send("evm_mine");
+            const aDaiBalContr1 = await aDai.balanceOf(aaveV3Interactions.address);
+            const aDaiBalC1Formatted = ethers.utils.formatEther(aDaiBalContr1);
+            console.log(`ADai balance of contract after supply is: ${ethers.utils.formatEther(aDaiBalContr1)}`);
+            await network.provider.send("evm_increaseTime", [360]);
+            await network.provider.send("evm_mine");
+            const aDaiBalContr2 = await aDai.balanceOf(aaveV3Interactions.address);
+            const aDaiBalC2Formatted = ethers.utils.formatEther(aDaiBalContr2);
+            console.log(`ADai balance of contract after supply later is: ${ethers.utils.formatEther(aDaiBalContr2)}`);
+            expect(Number(aDaiBalC1Formatted)).to.be.lessThan(Number(aDaiBalC2Formatted));
+        });
     });
 });
