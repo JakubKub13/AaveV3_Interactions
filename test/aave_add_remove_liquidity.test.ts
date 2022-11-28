d680288c56c7af44947fdf1bc6ce5dca98298172
JakubKub13 <jakubkubala3@gmail.com>
2022-11-28 14:08:50 +0100

    Deleting unused vars

--- test/aave_add_remove_liquidity.test.ts
+++ test/aave_add_remove_liquidity.test.ts
@@ -9,7 +9,7 @@ const AAVE_POOL_ADDRESSES_PROVIDERV3 = "0xa97684ead0e402dC232d5A977953DF7ECBaB3C
 const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
 const aDAI = "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE";
 const DAI_WHALE = "0x075e72a5eDf65F0A5f44699c7654C1a76941Ddc8";
-const AMOUNT_SUPPLY = 1000n * 10n ** 18n // 2000 DAI
+const AMOUNT_SUPPLY = 1000n * 10n ** 18n // 1000 DAI
 
 describe("Test Aave provide and withdraw liquidity", function () {
     let accounts: SignerWithAddress[];
