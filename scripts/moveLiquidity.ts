cf068456b86388a0945a39b9ab8d67e47b9b7242
JakubKub13 <jakubkubala3@gmail.com>
2022-12-13 11:12:25 +0100

    Adding values for scripts

--- scripts/moveLiquidity.ts
+++ scripts/moveLiquidity.ts
@@ -5,3 +5,17 @@ import { AaveV3Interactions } from "../typechain-types";
 import { developmentChains, networkConfig } from "../helper-hardhat-config";
 dotenv.config();
 import * as bridgeABI from "../externalABIs/Bridge.json";
+
+const AMOUNT_TO_FUND_ACCOUNT: BigNumber = ethers.utils.parseEther("100"); // 100 DAIs 
+
+async function main() {
+    const provider = new ethers.providers.JsonRpcProvider(process.env.POLYGON_MAINNET_RPC_URL);
+    const privateKey1 = process.env.PRIVATE_KEY;
+
+    let aaveV3Interactions: AaveV3Interactions;
+
+    const chainId = network.config.chainId;
+    const DaiAddr: string = networkConfig[chainId]["DAI"];
+    const AaveV3IntAddr: string = networkConfig[chainId][""]
+    const BridgeAddr: string = networkConfig[chainId][""];
+}
