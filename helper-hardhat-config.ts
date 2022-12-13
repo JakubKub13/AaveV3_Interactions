9a6ca2e815ad40abedfe0c750b4a63747456e5aa
JakubKub13 <jakubkubala3@gmail.com>
2022-12-13 10:43:19 +0100

    Setting up hardhat-helper-config

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,11 +1,9 @@
 export interface networkConfigItem {
     name?: string
-    auctionFactory?: string 
-    auctionImplementation1?: string
-    auctionImplementation2?: string
-    auctionImplementation3?: string
+    aaveV3Interactions?: string
     aDAI?: string
     DAI?: string
+    cBridge?: string
   }
 export interface networkConfigInfo {
     [key: number]: networkConfigItem
@@ -14,20 +12,34 @@ export interface networkConfigInfo {
 export const networkConfig: networkConfigInfo = {
     31337: {
         name: "localhost",
-        auctionFactory: "",
-        auctionImplementation1: "",
+        aaveV3Interactions: "",
+        DAI: "",
+        aDAI: "",
+        cBridge: "",
     },
 
-    5: {
-        name: "goerli",
+    42161: {
+        name: "arbitrumOne",
+        aaveV3Interactions: "",
+        DAI: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+        aDAI: "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE",
+        cBridge: "",
     },
 
-    80001: {
-        name: "mumbai",
+    10: {
+        name: "optimism",
+        aaveV3Interactions: "",
+        DAI: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+        aDAI: "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE",
+        cBridge: "",
     },
 
-    1: {
-        name: "mainnet",
+    250: {
+        name: "fantom",
+        aaveV3Interactions: "",
+        DAI: "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E",
+        aDAI: "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE",
+        cBridge: "0x374B8a9f3eC5eB2D97ECA84Ea27aCa45aa1C57EF",
     },
 }
 
