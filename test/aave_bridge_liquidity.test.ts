4160cac1e3741f2c5fb2ef1458f8f23ab00a1d47
JakubKub13 <jakubkubala3@gmail.com>
2022-12-16 10:00:07 +0100

    Preparing test env for testing bridge func

--- test/aave_bridge_liquidity.test.ts
+++ test/aave_bridge_liquidity.test.ts
@@ -9,7 +9,7 @@ const CBridge_ADDRESS: string = "0x88DCDC47D2f83a99CF0000FDF667A468bB958a78"; //
 const USDC: string = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"; //polygon
 const aUSDC: string = "0x625E7708f30cA75bfd92586e17077590C60eb4cD"; // polygon
 const USDC_WHALE: string = "0x9AC5637d295FEA4f51E086C329d791cC157B1C84"; // polygon
-const AMOUNT_SUPPLY = 1000n * 10n ** 18n // 1000 DAI
+const AMOUNT_SUPPLY = 1000n * 10n ** 6n // 1000 USDC
 
 describe("Bridging liquidity from Contract on source chain to contract on destination chain", () => {
     let accounts: SignerWithAddress[];
@@ -37,8 +37,15 @@ describe("Bridging liquidity from Contract on source chain to contract on destin
         await aaveV3Interactions.deployed();
     });
 
-    it("Contract should have been funded with usdc", async () => {
+    it("Contract should be set up to work with usdc", async () => {
+        expect(await aaveV3Interactions.getTokenAddress()).to.eq(USDC);
+    });
 
+    describe("Fund contract with USDC and Try to bridge funds to another chain", () => {
+        beforeEach(async () => {
+            const sendTx = await usdc.connect(accounts[0]).transfer(aaveV3Interactions.address, AMOUNT_SUPPLY);
+            await sendTx.wait();
+        });
     })
 
 })
