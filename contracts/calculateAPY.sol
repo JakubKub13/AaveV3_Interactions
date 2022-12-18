42bba93714b81653c43de24988e311d2ab639607
JakubKub13 <jakubkubala3@gmail.com>
2022-12-18 11:37:25 +0100

    Adding state vars

--- contracts/calculateAPY.sol
+++ contracts/calculateAPY.sol
@@ -1,6 +1,21 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
+
+import {IPoolDataProvider} from "@aave/core-v3/contracts/interfaces/IPoolDataProvider.sol";
+
 contract calculateAPY {
-    
+    IPoolDataProvider public aaveProtocolDataProvider;
+    uint256 public constant RAY = 10**27;
+    uint256 public constant SECONDS_PER_YEAR = 31536000;
+
+
+    constructor(address _aaveProtocolDataProvider) {
+        aaveProtocolDataProvider = IPoolDataProvider(_aaveProtocolDataProvider);
+    }
+
+
+
+
+
 }
