22c4b3720fb73cf21fc01f277216f98d42cb4a61
JakubKub13 <jakubkubala3@gmail.com>
2023-01-02 11:59:02 +0100

    Preparing notes

--- contracts/GelatoAutomationInteractions.sol
+++ contracts/GelatoAutomationInteractions.sol
@@ -3,5 +3,7 @@ pragma solidity 0.8.10;
 
 
 contract GelatoAutomationInteractions {
-    
+    // Implement Gelato smart contract to automate the process of supplying liquidity to Aave V3 on polygon
+    // We need to find exact function on Gelato and how to implement it in our smart contract
+    // We will try it first based on time interval than on conditions
 }
