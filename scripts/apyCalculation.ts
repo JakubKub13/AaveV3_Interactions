f0a3497c0290183a69964f67aad387b1a953bcc7
JakubKub13 <jakubkubala3@gmail.com>
2023-01-02 11:07:45 +0100

    Adding APY calculation script

--- scripts/apyCalculation.ts
+++ scripts/apyCalculation.ts
@@ -1,3 +1,14 @@
 // Creating script that will take variable from smart contract and calculate APY for given token pool
 import { ethers } from "hardhat";
 import { Contract } from "ethers";
+
+
+function getApy(liquidityrate: number) : number {
+    const RAY: number = 10 ** 27;
+
+    const depositAPR: number = liquidityrate / RAY;
+    const depositAPY: number = ((1 + (depositAPR / 31536000)) ** 31536000)) - 1;
+    
+    console.log(depositAPY);
+    return depositAPY;
+}
