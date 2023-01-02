1ea008b8445f6430752bf22f632bf7243fedd998
JakubKub13 <jakubkubala3@gmail.com>
2023-01-02 11:08:38 +0100

    Fixing bugs

--- scripts/apyCalculation.ts
+++ scripts/apyCalculation.ts
@@ -7,7 +7,7 @@ function getApy(liquidityrate: number) : number {
     const RAY: number = 10 ** 27;
 
     const depositAPR: number = liquidityrate / RAY;
-    const depositAPY: number = ((1 + (depositAPR / 31536000)) ** 31536000)) - 1;
+    const depositAPY: number = ((1 + (depositAPR / 31536000)) ** 31536000) - 1;
     
     console.log(depositAPY);
     return depositAPY;
