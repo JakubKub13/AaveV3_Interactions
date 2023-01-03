8aa2ca6972b928a2771e18706521528cc770048d
JakubKub13 <jakubkubala3@gmail.com>
2023-01-03 11:32:19 +0100

    Deleting file

--- contracts/GelatoAutomationInteractions.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
-
-
-contract GelatoAutomationInteractions {
-    // Implement Gelato smart contract to automate the process of supplying liquidity to Aave V3 on polygon
-    // We need to find exact function on Gelato and how to implement it in our smart contract
-    // We will try it first based on time interval than on conditions
-}
