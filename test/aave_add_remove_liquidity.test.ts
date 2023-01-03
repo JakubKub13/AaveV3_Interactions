f57d38d48bfe6c1a13b562269a21e77d42bfd183
JakubKub13 <jakubkubala3@gmail.com>
2023-01-03 12:14:16 +0100

    Fixing deployment bug

--- test/aave_add_remove_liquidity.test.ts
+++ test/aave_add_remove_liquidity.test.ts
@@ -6,6 +6,7 @@ import { AaveV3Interactions } from "../typechain-types";
 
 const AAVE_POOL_ADDRESSES_PROVIDERV3: string = "0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb"; //polygon
 const CBridge_ADDRESS: string = "0x88DCDC47D2f83a99CF0000FDF667A468bB958a78"; // Polygon
+const AAVE_PROTOCOL_DATA_PROVIDERV3: string = "0x69FA688f1Dc47d4B5d8029D5a35FB7a548310654";
 const DAI: string = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
 const aDAI: string = "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE";
 const DAI_WHALE: string = "0x075e72a5eDf65F0A5f44699c7654C1a76941Ddc8";
@@ -42,7 +43,7 @@ describe("Test Aave provide and withdraw liquidity", function () {
     describe("Depositing DAI to Aave", function() {
         beforeEach(async () => {
             const aaveV3InteractionsFactory = await ethers.getContractFactory("AaveV3Interactions");
-            aaveV3Interactions = await aaveV3InteractionsFactory.deploy(DAI, AAVE_POOL_ADDRESSES_PROVIDERV3, CBridge_ADDRESS);
+            aaveV3Interactions = await aaveV3InteractionsFactory.deploy(DAI, AAVE_POOL_ADDRESSES_PROVIDERV3, CBridge_ADDRESS, AAVE_PROTOCOL_DATA_PROVIDERV3);
             await aaveV3Interactions.deployed();
         });
 
