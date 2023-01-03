9b297ca1c4c493792766d9f869105b7edb26786f
JakubKub13 <jakubkubala3@gmail.com>
2023-01-03 12:01:29 +0100

    Fixing deploy bug

--- test/aave_bridge_liquidity.test.ts
+++ test/aave_bridge_liquidity.test.ts
@@ -35,7 +35,7 @@ describe("Bridging liquidity from Contract on source chain to contract on destin
         await usdc.connect(usdcWhale).transfer(accounts[0].address, AMOUNT_SUPPLY);
 
         const aaveV3InteractionsFactory = await ethers.getContractFactory("AaveV3Interactions");
-        aaveV3Interactions = await aaveV3InteractionsFactory.deploy(USDC,AAVE_POOL_ADDRESSES_PROVIDERV3, CBridge_ADDRESS, AAVE_PROTOCOL_DATA_PROVIDERV3);
+        aaveV3Interactions = await aaveV3InteractionsFactory.deploy(USDC, AAVE_POOL_ADDRESSES_PROVIDERV3, CBridge_ADDRESS, AAVE_PROTOCOL_DATA_PROVIDERV3);
         await aaveV3Interactions.deployed();
     });
 
